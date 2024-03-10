0cce4df1dc273e1dc9de38b5d1cd1f6f98a3f526
asynthe <bendunstan@protonmail.com>
2024-03-11 00:12:55 +0800

    update

--- hosts/untested/redteam/modules.nix
+++ hosts/untested/redteam/modules.nix
@@ -16,39 +16,29 @@
 	#./modules/desktop/i3.nix # In progress.
 	#./modules/desktop/xmonad.nix # In progress.
 
-	# Monitoring
-	#./modules/monitoring/elasticsearch.nix
-	#./modules/monitoring/grafana.nix
-	#./modules/monitoring/loki.nix
-	#./modules/monitoring/prometheus.nix
-
-
 	# Security Tool Box
 	# https://fabaff.github.io/nix-security-box/
 
-	# Chosen modules
-        ./modules/security/cloud.nix
-        ./modules/security/forensics.nix
-        ./modules/security/information-gathering.nix
-        ./modules/security/network.nix
-
-        # Modules
         #./modules/security/1_all.nix # Full package set.
         #./modules/security/bluetooth.nix
+        #./modules/security/cloud.nix
         #./modules/security/code.nix
         #./modules/security/container.nix
         #./modules/security/dns.nix
         #./modules/security/exploits.nix
+        #./modules/security/forensics.nix
         #./modules/security/fuzzers.nix
         #./modules/security/generic.nix
         #./modules/security/hardware.nix
         #./modules/security/host.nix
+        #./modules/security/information-gathering.nix
         #./modules/security/kubernetes.nix
         #./modules/security/ldap.nix
         #./modules/security/load-testing.nix
         #./modules/security/malware.nix
         #./modules/security/misc.nix
         #./modules/security/mobile.nix
+        #./modules/security/network.nix
         #./modules/security/packet-generators.nix
         #./modules/security/password.nix
         #./modules/security/port-scanners.nix
